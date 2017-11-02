5cf35a37cea6e73aedd08012ef8e1ca67baa1f22
Vishnu Babu <vb@one.com>
2017-11-02 11:15:27 +0400

    Read me

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# iTunesApp
+Welcome to iTunes App!
+===================
+
+This is a proxy app to iTunes search and lookup [API](https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/)
+
+Setup
+-------------
+
+clone the repository and from root folder run
+>npm run-script build
+>npm start
+
+Requires nodejs 8.8.1 (latest)  -> using (async / await), promisify
+
+Backend
+-------------
+Supports 2 apis
+>/api/search
+>/api/lookup
+
+These in turn uses the iTunes API
+
+There is a caching layer for all iTunes Requests
+https://www.npmjs.com/package/node-cache
+
+There is a caching layer for all iTunes Requests
+Uses [node-cache](https://www.npmjs.com/package/node-cache)
+
+For input request validations using [express-validator](https://github.com/ctavan/express-validator)
+
+Frontend
+-------------
+Its a React App. It proxies all '/api' requests to the Backend.
+Its in the folder [./client](https://github.com/Vishnubabu/iTunesApp/tree/master/client)
+Used - [create-react-app](https://github.com/facebookincubator/create-react-app) for creation
+
+It has 3 react components
+
+> - SearchBox
+> - LookupBox
+> - ItemResults
+
+Uses Routes from [react-router](https://github.com/ReactTraining/react-router)
+search and lookup actions changes the routes and the components listen to the route changes and fetch data from the backend
+
+Uses [redux-promise-middleware](https://github.com/pburtchaell/redux-promise-middleware/blob/master/docs/introduction.md) for handling async requests
+
+Uses [React-Bootstrap](https://react-bootstrap.github.io/) for these components
+
+- Table
+- Forms
+- Tabs
+- Button
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,29 +11,27 @@ const getTabFromRoute = route => {
     return (match && match[1]) || 'search';
 }
 
-export default ({ history }) => {
-    return (
-        <div>
-            <Tabs activeKey={ getTabFromRoute(history.location.pathname) } id="maintabs" onSelect={ tab => history.push('/' + tab) }>
-                <Tab eventKey={'search'} title="Search">
-                    <Route path="/search/:term?/:media?/:entity?" render={ ({match}) => (
-                        <div className="well">
-                            <SearchBox { ...{match, history} } />
-                        </div>
-                    )} />
-                </Tab>
-                <Tab eventKey={'lookup'} title="Lookup">
-                    <Route path="/lookup/:store?/:id?" render={ ({match}) => (
-                        <div className="well">
-                            <LookupBox { ...{match, history} } />
-                        </div>
-                    )} />
-                </Tab>
-            </Tabs>
+export default ({ history }) => (
+    <div>
+        <Tabs activeKey={ getTabFromRoute(history.location.pathname) } id="maintabs" onSelect={ tab => history.push('/' + tab) }>
+            <Tab eventKey={'search'} title="Search">
+                <Route path="/search/:term?/:media?/:entity?" render={ ({match}) => (
+                    <div className="well">
+                        <SearchBox { ...{match, history} } />
+                    </div>
+                )} />
+            </Tab>
+            <Tab eventKey={'lookup'} title="Lookup">
+                <Route path="/lookup/:store?/:id?" render={ ({match}) => (
+                    <div className="well">
+                        <LookupBox { ...{match, history} } />
+                    </div>
+                )} />
+            </Tab>
+        </Tabs>
 
-            <div className="container-fluid">
-                <ItemResults/>
-            </div>
+        <div className="container-fluid">
+            <ItemResults/>
         </div>
-    );
-};
+    </div>
+);
